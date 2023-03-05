3f0828eeef7018fc0449415c1fe91a08caa4a1c9
rohitverse <rohitkr23072001@gmail.com>
2023-03-05 16:57:26 +0530

    create nft commit

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Button from './Button';
+import Banner from './Banner';
+import CreatorCard from './CreatorCard';
+import NFTCard from './NFTCard';
+import Input from './Input';
+
+export { Navbar, Banner, Footer, Button, CreatorCard, NFTCard, Input };
---
+++ pages/listed-nfts.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const creatednfts = () => <div> Created NFTs</div>;
+
+export default creatednfts;
